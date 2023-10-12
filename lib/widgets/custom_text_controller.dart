1feaf7e22c6b04ec9687117677f3f2cd5b75d88f
Toi1452002 <dangthanhtoi1452002@gmail.com>
2023-10-12 10:46:11 +0700

    controller

--- lib/widgets/custom_text_controller.dart
+++ lib/widgets/custom_text_controller.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 List<int> indexError = [];
-
+String textBefore = "";
 class CustomTextController extends TextEditingController {
   @override
   TextSpan buildTextSpan(
@@ -9,6 +9,17 @@ class CustomTextController extends TextEditingController {
       required bool withComposing}) {
     List<TextSpan> children = [];
     List<String> textList = text.split(".");
+    List<String> textbfList = textBefore.split(".");
+    if(text.isEmpty) indexError.clear();
+    if(textBefore.isNotEmpty && textbfList.length!=textList.length){
+      int i = value.selection.start;
+      int index = text.substring(0,i).split(".").length-1;
+      if(textbfList.length<textList.length){
+        indexError = indexError.map((e)=>index <= e ? e+=1 : e).toList();
+      }else{
+        indexError = indexError.map((e)=>index < e ? e-=1 : e).toList();
+      }
+    }
     if(indexError.isNotEmpty){
       for(int k = 0;k<textList.length;k++){
         if(indexError.contains(k)){
@@ -26,6 +37,7 @@ class CustomTextController extends TextEditingController {
     }else{
       children.add(TextSpan(text: text,style: style));
     }
+    textBefore = text;
     return TextSpan(children: children, style: style);
   }
 }
